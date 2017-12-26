87499005f4fe7d31c20df543badb6c590de0186e
Akshay Saini <anonyxhappie@gmail.com>
2017-12-27 03:20:32 +0530

    minor improvements

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ $(document).ready(() => {
 });
 
 function getMovies(searchText){
+    $('#latestText').css('display', 'none');
     axios.get('http://www.omdbapi.com?s='+searchText+'&apikey=b3d8145e')
     .then((response) => {
         console.log(response);
@@ -69,7 +70,7 @@ function getMovie(){
             <div class="row">
                 <div class="col-md-12">
                     <hr />
-                    <h3>Plot</h3>
+                    <h3>Overview</h3>
                     ${movie.Plot}
                     <hr />
                     <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
@@ -83,4 +84,77 @@ function getMovie(){
     .catch((err) => {
         console.log(err);
     });
+}
+
+function getNowPlaying(){
+    axios.get('https://api.themoviedb.org/3/movie/now_playing?api_key=3a6b37f95c6a73b59ec674e0c91b2156&language=en-US&page=1')
+    .then((response) => {
+        console.log(response);
+        let movies = response.data.results;
+        let output = '';
+        $.each( movies, (index, movie) => {    
+            output += `
+                <div class="col-md-3">
+                    <div class="well text-center">
+                        <img src="${'http://image.tmdb.org/t/p/w185' + movie.poster_path}" />
+                        <h5>${movie.original_title}</h5>
+                        <a onclick="movieSelected('${movie.id}')" class="btn btn-primary" href="#">Movie Details</a>
+                    </div>
+                </div>
+            `;
+        });
+        $('#movies').html(output);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+}
+
+function getMovieSelected(){
+    let movieId = sessionStorage.getItem('movieId');
+    axios.get('https://api.themoviedb.org/3/movie/'+ movieId +'?api_key=3a6b37f95c6a73b59ec674e0c91b2156&language=en-US&page=1')
+    .then((response) => {
+        console.log(response);
+        let movie = response.data;
+        if(movie.homepage == "") 
+        movie.homepage = "N/A";
+        let output = `
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="${'http://image.tmdb.org/t/p/w185' + movie.poster_path}" class="thumbnail">
+                </div>
+                <div class="col-md-8">
+                    <h2>${movie.original_title}</h2>
+                    <ul class="list-group">
+                    <li class="list-group-item"><strong>Genre:</strong> ${movie.genres[0].name}</li>
+                    <li class="list-group-item"><strong>Released:</strong> ${movie.release_date}</li>
+                    <li class="list-group-item"><strong>Runtime:</strong> ${movie.runtime}mins</li>
+                    <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.vote_average}/10</li>
+                    <li class="list-group-item"><strong>Production:</strong> ${movie.production_companies[0].name}</li>
+                    <li class="list-group-item"><strong>Revenue:</strong> ${movie.revenue/1000000}M</li>
+                    <li class="list-group-item"><strong>Homepage:</strong> ${movie.homepage}</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <hr />
+                    <h3>Overview</h3>
+                    ${movie.overview}
+                    <hr />
+                    <a href="http://imdb.com/title/${movie.imdb_id}" target="_blank" class="btn btn-primary">View IMDB</a>
+                    <a href="index.html" class="btn btn-default">Go Back To Search</a>
+                    <hr />
+                </div>
+            </div>
+        `;
+        $('#movie').html(output);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+}
+
+function clearSession(){
+    sessionStorage.clear();
 }
