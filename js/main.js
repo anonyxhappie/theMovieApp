3395c7d8b7f26ce5cbcac641c90c8bd0f53ebc07
Akshay Saini <anonyxhappie@gmail.com>
2017-12-31 05:32:43 +0530

    optimised

--- js/main.js
+++ js/main.js
@@ -1,77 +1,62 @@
-$('img').hover().toggleClass("animated pulse"); 
+$('.container').toggleClass("animated pulse"); 
+$('img').hover(
+    function(){$(this).addClass("animated pulse")},
+    function(){$(this).removeClass("animated pulse")}
+);     
+
 /**
  * For index.html
  */
 $(document).ready(() => {
-    getLatestMovies();
-    getTopRatedMovies();
-    
+    getMovies('now_playing', 'default');
+    getMovies('top_rated', 'default');
     $('#searchForm').on('submit', (e) => {
         let searchText = $('#searchText').val();
         sessionStorage.setItem('searchText', searchText);
-        getSearchMovies(searchText);
+        getMovies('search', searchText);
         e.preventDefault();
     });
 });
 
-function getLatestMovies(){
-    axios.get('https://api.themoviedb.org/3/movie/now_playing?api_key=3a6b37f95c6a73b59ec674e0c91b2156&language=en-US&page=1')
+function getMovies(keyword, searchText){
+    let url = '', id = '';
+    let movieId = sessionStorage.getItem('movieId');
+    if(keyword == 'now_playing'){
+        url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=3a6b37f95c6a73b59ec674e0c91b2156&language=en-US&page=1';
+        id = '#latestMovies';
+    }else if(keyword == 'top_rated'){
+        url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=3a6b37f95c6a73b59ec674e0c91b2156&language=en-US&page=1';
+        id = '#topRatedMovies';
+    }else if(keyword == 'recommended'){
+        url = 'https://api.themoviedb.org/3/movie/' + movieId + '/recommendations?api_key=3a6b37f95c6a73b59ec674e0c91b2156&language=en-US&page=1';
+        id = '#recommendedMovies';
+    }else if(keyword == 'search'){
+        $('.textHeading').css('display', 'none');
+        $('#latestMovies').css('display', 'none');
+        $('#topRatedMovies').css('display', 'none');
+        url = 'https://api.themoviedb.org/3/search/movie?api_key=3a6b37f95c6a73b59ec674e0c91b2156&language=en-US&page=1&include_adult=false&query=' + searchText;
+        id = '#searchMovies';
+    }else{
+        return false;
+    }
+    axios.get(url)
     .then((response) => {
         console.log(response);
         let movies = response.data.results;
-        let output = '';
-        $.each( movies, (index, movie) => {    
-            output += `
-                <div class="animated rotateIn">
-                    <img onclick="movieSelected('${movie.id}')" src="${'http://image.tmdb.org/t/p/w185' + movie.poster_path}" />
-                </div>
-            `;
-        });
-        $('#latestMovies').html(output);
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-}
-
-function getTopRatedMovies(){
-    axios.get('https://api.themoviedb.org/3/movie/top_rated?api_key=3a6b37f95c6a73b59ec674e0c91b2156&language=en-US&page=1')
-    .then((response) => {
-        console.log(response);
-        let movies = response.data.results;
-        let output = '';
-        $.each( movies, (index, movie) => {    
-            output += `
-                <div class="animated rotateIn">
-                    <img onclick="movieSelected('${movie.id}')" src="${'http://image.tmdb.org/t/p/w185' + movie.poster_path}" />
-                </div>
-            `;
-        });
-        $('#topRatedMovies').html(output);
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-}
-
-function getSearchMovies(searchText){
-    $('.textHeading').css('display', 'none');
-    axios.get('http://www.omdbapi.com?s='+searchText+'&apikey=b3d8145e')
-    .then((response) => {
-        console.log(response);
-        let movies = response.data.Search;
-        let output = '';
+        let output = '', imgUrl = '';
         $.each( movies, (index, movie) => {
-            //console.log(movie.Poster);
-            if(movie.Poster == "N/A") 
-                movie.Poster = "img/image-error.png";
+            console.log(movie.poster_path);    
+            if(movie.poster_path) 
+                imgUrl = 'http://image.tmdb.org/t/p/w185' + movie.poster_path;
+            else
+                imgUrl= 'img/image-error.png';
             output += `
                 <div class="animated rotateIn">
-                    <img onclick="movieSelected('${movie.imdbID}')" src="${movie.Poster}" />
+                    <img onclick="movieSelected('${movie.id}')" src="${imgUrl}" />
                 </div>
             `;
         });
-        $('#searchMovies').html(output);
+        $(id).html(output);
     })
     .catch((err) => {
         console.log(err);
@@ -87,72 +72,37 @@ function movieSelected(id){
  * For movies.html
  */
 
-function getMovie(){
-    let movieId = sessionStorage.getItem('movieId');
-    axios.get('http://www.omdbapi.com?i='+movieId+'&apikey=b3d8145e')
-    .then((response) => {
-        console.log(response);
-        let movie = response.data;
-        if(movie.Poster == "N/A") 
-            movie.Poster = "img/image-error.png";
-        let output = `
-            <div class="row">
-                <div class="col-md-4">
-                    <img src="${movie.Poster}" class="thumbnail">
-                </div>
-                <div class="col-md-8">
-                    <h2>${movie.Title}</h2>
-                    <ul class="list-group">
-                    <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
-                    <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
-                    <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
-                    <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
-                    <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
-                    <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
-                    <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
-                    </ul>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-12">
-                    <hr />
-                    <h3>Overview</h3>
-                    ${movie.Plot}
-                    <hr />
-                    <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
-                    <a href="index.html" class="btn btn-default">Go Back To Search</a>
-                    <hr />
-                </div>
-            </div>
-        `;
-        $('#movie').html(output);
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-}
-
  function getMovieSelected(){
     let movieId = sessionStorage.getItem('movieId');
+    let imgUrl = 'img/image-error.png';
     axios.get('https://api.themoviedb.org/3/movie/'+ movieId +'?api_key=3a6b37f95c6a73b59ec674e0c91b2156&language=en-US&page=1')
     .then((response) => {
         console.log(response);
         let movie = response.data;
         if(movie.homepage == "") 
-        movie.homepage = "N/A";
+            movie.homepage = "N/A";
+        if(movie.poster_path) 
+            imgUrl = 'http://image.tmdb.org/t/p/w185' + movie.poster_path;
+            let pclist = '', genrelist = '';
+            movie.production_companies.forEach(element => {
+                pclist += element.name + ' ';
+            });
+            movie.genres.forEach(element => {
+                genrelist += element.name + ' ';
+            });
         let output = `
             <div class="row">
                 <div class="col-md-4">
-                    <img src="${'http://image.tmdb.org/t/p/w185' + movie.poster_path}" class="thumbnail">
+                    <img src="${imgUrl}" class="thumbnail">
                 </div>
                 <div class="col-md-8">
                     <h2>${movie.original_title}</h2>
                     <ul class="list-group">
-                    <li class="list-group-item"><strong>Genre:</strong> ${movie.genres[0].name}</li>
+                    <li class="list-group-item"><strong>Genre:</strong> ${genrelist}</li>
                     <li class="list-group-item"><strong>Released:</strong> ${movie.release_date}</li>
                     <li class="list-group-item"><strong>Runtime:</strong> ${movie.runtime}mins</li>
                     <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.vote_average}/10</li>
-                    <li class="list-group-item"><strong>Production:</strong> ${movie.production_companies[0].name}</li>
+                    <li class="list-group-item"><strong>Production:</strong> ${pclist}</li>
                     <li class="list-group-item"><strong>Revenue:</strong> ${movie.revenue/1000000}M</li>
                     <li class="list-group-item"><strong>Homepage:</strong> ${movie.homepage}</li>
                     </ul>
@@ -171,28 +121,6 @@ function getMovie(){
             </div>
         `;
         $('#movie').html(output);
-        getRecommendedMovies();
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-}
-
-function getRecommendedMovies(){
-    let movieId = sessionStorage.getItem('movieId');
-    axios.get('https://api.themoviedb.org/3/movie/' + movieId + '/recommendations?api_key=3a6b37f95c6a73b59ec674e0c91b2156&language=en-US&page=1')
-    .then((response) => {
-        console.log(response);
-        let movies = response.data.results;
-        let output = '';
-        $.each( movies, (index, movie) => {    
-            output += `
-                <div class="animated rotateIn">
-                    <img onclick="movieSelected('${movie.id}')" src="${'http://image.tmdb.org/t/p/w185' + movie.poster_path}" />
-                </div>
-            `;
-        });
-        $('#recommendedMovies').html(output);
     })
     .catch((err) => {
         console.log(err);
